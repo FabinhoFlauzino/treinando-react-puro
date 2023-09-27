4b3ca51a1521a7d0840cc6c5cd948743a7fce490
Fabio Flauzino <fabio@mfrural.com.br>
2023-09-27 17:41:40 -0300

    finalizando página contato

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Produtos from "./Components/Produtos";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Contato from "./Components/Contato";
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="contato" element={<Contato />}/>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
